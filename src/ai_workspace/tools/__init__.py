0075ea68c240b689f9aa8bd822c3cfc1025001d6
Davi Azevedo <daviaaze@gmail.com>
2026-06-15 18:45:52 -0300

    feat: CLI commands for all tools (fetch, browser, ml, olx, scrape) + pi extension updated

--- src/ai_workspace/tools/__init__.py
+++ src/ai_workspace/tools/__init__.py
@@ -5,8 +5,6 @@ from ai_workspace.tools.marketplace import MercadoLivreSearchTool, OLXSearchTool
 from ai_workspace.tools.headless_browser import HeadlessBrowserTool
 from ai_workspace.tools.paginated_scraper import PaginatedScraperTool
 
-from ai_workspace.tools.paginated_scraper import PaginatedScraperTool
-
 __all__ = [
     "WebFetchTool",
     "MercadoLivreSearchTool",
